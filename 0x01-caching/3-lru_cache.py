01370f2964adc19395eb48d40b3651d420f100a7
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-12-22 21:31:02 +0300

    LRUCache caching system

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -17,38 +17,25 @@ class LRUCache(BaseCaching):
         """
         super().__init__()
         self.cache_data = OrderedDict()
-        self.count = {}
 
     def put(self, key, item):
         """assign to the {} self.cache_data the item value for the key
         """
         if key and item:
             if len(self.cache_data) < BaseCaching.MAX_ITEMS:
-                if key in self.cache_data.keys():
-                    self.count[key] += 1
-                else:
-                    self.count[key] = 0
                 self.cache_data[key] = item
             else:
-                if key in self.cache_data.keys():
-                    self.count[key] += 1
-                else:
-                    self.count[key] = 0
+                if key not in self.cache_data:
+                    keys = list(self.cache_data.keys())
+                    print("DISCARD: {}".format(keys[0]))
+                    del self.cache_data[keys[0]]
                 self.cache_data[key] = item
-                keys = list(self.cache_data.keys())
-                lru = keys[0]
-                for k, v in self.count.items():
-                    if v < self.count[lru]:
-                        lru = k
-                if len(keys) > BaseCaching.MAX_ITEMS:
-                    print("DISCARD: {}".format(lru))
-                    del self.count[lru]
-                    del self.cache_data[lru]
+                self.cache_data.move_to_end(key)
 
     def get(self, key):
         """return value in self.cache_data linked to key
         """
         if not key or not self.cache_data.get(key):
             return None
-        self.count[key] += 1
+        self.cache_data.move_to_end(key)
         return self.cache_data.get(key)
